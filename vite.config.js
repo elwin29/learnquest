61386989e15d44183410d2bd9d73045589beb587
elwin29 <vladimir.leonard@binus.ac.id>
2024-11-26 19:59:01 +0700

    update vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.js"], // Input files for Vite
+            refresh: true, // Enable auto-refresh during development
         }),
     ],
+    build: {
+        outDir: "public/build", // Output the build files to the 'public/build' directory
+        manifest: true, // This will generate a manifest file that Vercel needs
+    },
 });
