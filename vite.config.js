2fdc729bec16d28dd8c290ad6f88bd4d78b558b1
elwin29 <vladimir.leonard@binus.ac.id>
2024-11-26 21:55:11 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,6 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: "dist", // You can use 'dist' here or 'public/build' for Laravel compatibility
-        manifest: true, // This ensures proper linking of assets
-        emptyOutDir: true, // Clear output directory before each build
+        outDir: "public/build", // Set the build output directory
     },
 });
